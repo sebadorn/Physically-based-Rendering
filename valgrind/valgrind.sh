d0cc5834bc4f51ae55922d80d29c94687d847857
sebadorn <seba_dorn@gmx.de>
2014-06-08 15:23:40 +0200

    Accidently added the whole project again.

--- valgrind/valgrind.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# --gen-suppressions=all
-
-valgrind --tool=memcheck --leak-check=full --show-reachable=yes --suppressions=./valgrind/valgrind.supp --log-file=./valgrind/valgrind.log ./PBR $@
